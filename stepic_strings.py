6846362bd3301604b097675799dd646f34ebc1ca
root <inorect.a.z@gmail.com>
2019-03-29 15:53:43 +0300

    First try

--- stepic_strings.py
+++ stepic_strings.py
@@ -6,11 +6,15 @@ result_string = ''
 for symbol in string:
     if string.index(symbol) == 0 and string.index(symbol) != k:
         substring = substring + symbol
+        len = string.index(symbol)
     if symbol == string[string.index(symbol) - 1] and string.index(symbol) != 0 and string.index(symbol) != k:
         substring = substring + symbol
+        len = string.index(symbol)
     if symbol != string[string.index(symbol) - 2] and string.index(symbol) != k:
         result_string = result_string + string[string.index(symbol) - 1] + str(len(substring))
         substring = ''
+        len = string.index(symbol)
     if string.index(symbol) == k:
         result_string = result_string + symbol + str(len(substring))
+        len = string.index(symbol)
 print(result_string)
