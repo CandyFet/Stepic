cf9513fdc9baa32b708910abd6b8d173faf6082e
root <inorect.a.z@gmail.com>
2019-03-29 22:10:48 +0300

    cherez funciyuy

--- stepic_strings.py
+++ stepic_strings.py
@@ -1,20 +1,27 @@
-string = 'abct'
-i = 1
-k = int(len(string) - 1)
+def substring_counting(substring):
+    length = len(substring)
+    sub_builder = substring[0] + str(length)  # type: string
+    return sub_builder
+
+
+string = 'asdasd'
+i = 0
+k = len(string) - 1
 substring = ''
-result_string = ''
-for symbol in string:
-    if string.index(symbol) == 0 and string.index(symbol) != k:
-        substring = substring + symbol
-        len = string.index(symbol)
-    if symbol == string[string.index(symbol) - 1] and string.index(symbol) != 0 and string.index(symbol) != k:
-        substring = substring + symbol
-        len = string.index(symbol)
-    if symbol != string[string.index(symbol) - 2] and string.index(symbol) != k:
-        result_string = result_string + string[string.index(symbol) - 1] + str(len(substring))
+res_string = ''
+while i < k:
+    if string[i] == string[i - 1]:
+        substring = substring + string[i]
+        i += 1
+    if string[i] != string[i - 1] and i != 0:
+        substring = substring + string[i]
+        res_string = res_string + substring_counting(substring)
         substring = ''
-        len = string.index(symbol)
-    if string.index(symbol) == k:
-        result_string = result_string + symbol + str(len(substring))
-        len = string.index(symbol)
-print(result_string)
+        i += 1
+    if i == 0:
+        substring = substring + string[i]
+        i += 1
+    if string[i] != string[i - 1] and i-1 == 0:
+        res_string = res_string + substring_counting(substring)
+        substring = ''
+print(res_string)
