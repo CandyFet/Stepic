6959687bb562c7bc0f5a5270836d4c7ddde5e26f
root <inorect.a.z@gmail.com>
2019-03-30 00:26:22 +0300

    cherez funciyuy

--- stepic_strings.py
+++ stepic_strings.py
@@ -1,27 +1,11 @@
-def substring_counting(substring):
-    length = len(substring)
-    sub_builder = substring[0] + str(length)  # type: string
-    return sub_builder
-
-
-string = 'asdasd'
-i = 0
-k = len(string) - 1
-substring = ''
-res_string = ''
-while i < k:
-    if string[i] == string[i - 1]:
-        substring = substring + string[i]
-        i += 1
-    if string[i] != string[i - 1] and i != 0:
-        substring = substring + string[i]
-        res_string = res_string + substring_counting(substring)
-        substring = ''
-        i += 1
-    if i == 0:
-        substring = substring + string[i]
-        i += 1
-    if string[i] != string[i - 1] and i-1 == 0:
-        res_string = res_string + substring_counting(substring)
-        substring = ''
-print(res_string)
+s = input()
+cnt = 1
+for i in range(len(s)-1):
+	if s[i] == s[i+1]:
+		cnt += 1
+	else:
+		print(s[i], end='')
+		print(cnt, end='')
+		cnt=1
+print(s[-1], end='')
+print(cnt, end='')
